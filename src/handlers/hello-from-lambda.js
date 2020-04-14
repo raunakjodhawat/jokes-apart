bcb0f1e216f65da596725ef5a6cdd7a4c6f79c66
Raunak Jodhawat <raunakjodhawat@gmail.com>
2020-04-13 21:47:21 -0400

    Change funciton name, and simplify codebase

--- src/handlers/hello-from-lambda.js
+++ src/handlers/hello-from-lambda.js
@@ -2,7 +2,7 @@
  * A Lambda function that returns a string.
  */
 let axios = require('axios');
-exports.helloFromLambdaHandler = async () => {
+exports.getRandomJokes = async () => {
     let res = await axios.get('http://api.icndb.com/jokes/random/')
     if(res && res.data && res.data.value && res.data.value.joke){
         return {
